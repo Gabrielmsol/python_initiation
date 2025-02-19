9edcbfe077ae4f772e3f376e0d2b68db5730f193
Gabrielmsol <gabrielmsol37@gmail.com>
2025-02-19 17:48:43 -0300

    More organized

--- app/utils/google_utils.py
+++ app/utils/google_utils.py
@@ -7,28 +7,33 @@
 # e crie a sua chave.
 # %%
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import os
 
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
+class Email(BaseModel):
+    subject: str
+    sender: str
+    received_date: str
+
+class Analysis(BaseModel):
+    summary: str
+    keywords: list[str]
+    sentiment: str
+
+class EmailInfo(BaseModel):
+    email: Email
+    analysis: Analysis
+    
 instructions = """
 The please write down in brazilian portuguese, follow the instructions:
 You will receive data in JSON format. Please parse the data and organize it into a response following this structure:
-{
-    "email": {
-        "subject": "<email_subject>",
-        "sender": "<sender_email>",
-        "received_date": "<date_received>"
-    },
-    "analysis": {
-        "summary": "<brief_summary_of_email>",
-        "keywords": ["<keyword1>", "<keyword2>"],
-        "sentiment": "<positive/neutral/negative>"
-    }
-}
+
 Please ensure that your response follows this exact structure, with valid JSON syntax.
 """  # Poe as informações do jeito que quiser
 
@@ -36,9 +41,15 @@ client = genai.Client(api_key=GOOGLE_API_KEY)
 
 
 def analyze_with_ai(data: dict) -> dict:
+  
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=[instructions, str(data)]
+    model='gemini-2.0-flash-001',
+    contents=instructions,
+    config=types.GenerateContentConfig(
+        response_mime_type='application/json',
+        response_schema=EmailInfo,
+        ),
     )
+    
     return response.to_json_dict()
     
